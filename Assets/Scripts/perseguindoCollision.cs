0b7fd364d41cc5326e044b922320e18a40a34d0e
felipevk@gmail.com <felipevk@gmail.com@89ab6b84-4d4b-9379-56b6-cb210b568133>
2014-01-25 20:46:08 +0000

    Script para receber colisoes enquanto no raio de perseguição do enfermeiro

---
+++ Assets/Scripts/perseguindoCollision.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class perseguindoCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+		//dar um nome melhor ao objeto que possui esse collider
+		if(other.gameObject.name.Equals("sprite")){
+			enfermeiroScript.triggerFollow = true;
+		}
+	}
+	
+	public void OnTriggerExit2D(Collider2D other){
+		//dar um nome melhor ao objeto que possui esse collider
+		if(other.gameObject.name.Equals("sprite")){
+			enfermeiroScript.triggerFollow = false;
+		}
+	}
+}
+
