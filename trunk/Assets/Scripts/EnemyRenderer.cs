000908480259fc242b5d094406476ebe380dec00
felipevk@gmail.com <felipevk@gmail.com@89ab6b84-4d4b-9379-56b6-cb210b568133>
2014-01-25 05:48:59 +0000

    Script para renderizacao do sprite do inimigo dependendo de como ele está sendo visto

---
+++ trunk/Assets/Scripts/EnemyRenderer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Domain{Real,Imaginary}
+public class EnemyRenderer : MonoBehaviour {
+	public Domain _domainType = Domain.Real;
+	public Sprite _spriteImag;
+	public Sprite _spriteReal;
+	// Use this for initialization
+	void Start () {
+		//codigo teste para mover o inimigo no inicio
+		rigidbody2D.AddForce(new Vector2(6.0f,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+
+	void checkVision(){
+		SpriteRenderer thisSpriteRenderer = renderer as SpriteRenderer;
+		switch(_domainType){
+		case Domain.Imaginary:
+			thisSpriteRenderer.sprite = _spriteImag;
+			//depois inserir um codigo decente para troca de sprites
+			break;
+		case Domain.Real:
+			thisSpriteRenderer.sprite = _spriteReal;
+			//depois inserir um codigo decente para troca de sprites
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void setDomain(Domain newDomain){
+		_domainType = newDomain;
+		checkVision();
+		//fazer mais coisas como fade in ou fade out
+	}
+
+	public void move(){
+		float speed = 20.0f;
+		if (transform.position.x < -10) {
+			//turn around
+			//transform.Rotate(new Vector3(0,180,0));
+			//speed = -speed;
+			//transform.Translate(new Vector3(0, -transform.position.y,0) );
+			rigidbody2D.AddForce(new Vector2(speed,0));
+		}
+		if(transform.position.x > 16){
+			rigidbody2D.AddForce(new Vector2(-speed,0));
+		}
+		//transform.Translate( new Vector3(speed*Time.deltaTime,0,0) );
+	}
+}
