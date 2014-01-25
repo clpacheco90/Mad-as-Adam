c84c2f81b0e1ecf1f3bb7426e39fb370ccab9e89
felipevk@gmail.com <felipevk@gmail.com@89ab6b84-4d4b-9379-56b6-cb210b568133>
2014-01-25 20:46:40 +0000

    Script para receber colisoes enquanto no raio de observação do enfermeiro

---
+++ trunk/Assets/Scripts/desconfiadoCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class desconfiadoCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+		//dar um nome melhor ao objeto que possui esse collider
+		if(other.gameObject.name.Equals("sprite")){
+
+			enfermeiroScript.triggerDesconfiada = true;
+		}
+
+	}
+
+	public void OnTriggerExit2D(Collider2D other){
+		//dar um nome melhor ao objeto que possui esse collider
+		if(other.gameObject.name.Equals("sprite")){
+			enfermeiroScript.triggerDesconfiada = false;
+		}
+	}
+}
