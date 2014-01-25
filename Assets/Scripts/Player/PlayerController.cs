6def35da95638ffc92f2037a61094cd0fcfc84bf
pacheco@calvinpacheco.com <pacheco@calvinpacheco.com@89ab6b84-4d4b-9379-56b6-cb210b568133>
2014-01-25 21:18:09 +0000

    fixed Player/PlayerController.cs ROTATION

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,35 +9,24 @@ public class PlayerController : MonoBehaviour {
     delegate void Controllers(CharacterController2D c);
 	private Controllers _controllers;
 
-	public bool isCrounch = false;
+    //!Erase later
+    public bool isCrounch;
     //-----------------------------------------------------------------------------------------------------------------------------//	
 	void Awake () {
         _character2D.StartControllers(this.gameObject);
         _controllers += CharacterMovement.GravityMovementX;
-        //_controllers += CharacterMovement.GravityMovementY;
+        _controllers += CharacterMovement.ApplySquatDown;
         _controllers += CharacterMovement.ApplyGravity;
         _controllers += CharacterMovement.ApplyJumping;
         _controllers += CharacterMovement.RefreshMovement;
 	}
     //-----------------------------------------------------------------------------------------------------------------------------//	
-	void Update () {
-
-		//if(Input.GetAxis())
-
-	
-        if (_character2D.Movement.direction.sqrMagnitude > 0.01) _controllers += CharacterMovement.ApplyRotation;
-        else _controllers -= CharacterMovement.ApplyRotation;
-            //CharacterMovement.ApplyRotation(ref _character2D.Movement, ref _quaternionY, _attributes.canControl);
-        //NGUIDebug.Log(_controllerMovement.direction.ToString());        
-        //float v;
-        //CharacterMovement.IsMovingVertical(out v, true);
-        //Debug.Log(v);
+	void Update () {		
+        if(CharacterMovement.IsMoving()) _controllers += CharacterMovement.ApplyRotation;
         _character2D.UpdateControllers();		
 	}
     //-----------------------------------------------------------------------------------------------------------------------------//	
 	void FixedUpdate(){
-        //_controllers(_character2D.Movement, _character2D.Jump, _character2D.Controller);
-        
         _controllers(_character2D);
 	}
     //-----------------------------------------------------------------------------------------------------------------------------//	
