05ccae2d8ffc8f169a5ee8c96e07bd7f61f4ec40
pacheco@calvinpacheco.com <pacheco@calvinpacheco.com@89ab6b84-4d4b-9379-56b6-cb210b568133>
2014-01-25 01:19:22 +0000

    first build

---
+++ trunk/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    //-----------------------------------------------------------------------------------------------------------------------------//	
+    public CharacterController2D _character2D = new CharacterController2D();
+
+    //delegate void Controllers(ControllerMovement m, ControllerJumping j, CharacterController c);
+    delegate void Controllers(CharacterController2D c);
+	private Controllers _controllers;
+    //-----------------------------------------------------------------------------------------------------------------------------//	
+	void Awake () {
+        _character2D.StartControllers(this.gameObject);
+        _controllers += CharacterMovement.GravityMovementX;
+        _controllers += CharacterMovement.ApplyGravity;
+        _controllers += CharacterMovement.ApplyJumping;
+		_controllers += CharacterMovement.RefreshMovement;
+	}
+    //-----------------------------------------------------------------------------------------------------------------------------//	
+	void Update () {
+        //NGUIDebug.Log(_controllerMovement.direction.ToString());        
+        _character2D.UpdateControllers();		
+	}
+    //-----------------------------------------------------------------------------------------------------------------------------//	
+	void FixedUpdate(){
+        //_controllers(_character2D.Movement, _character2D.Jump, _character2D.Controller);
+        _controllers(_character2D);
+	}
+    //-----------------------------------------------------------------------------------------------------------------------------//	
+
+}
